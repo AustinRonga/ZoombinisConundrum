ee2d98229d88357b7ba32e312eb37ddbd46b2629
BunterTheMage <anonjosephovich@gmail.com>
2020-07-05 10:50:41 -0400

    Created full set of ZBs, sub-set of 16, and attempted to have it compare similarities

--- src/include/Zoombinis.h
+++ src/include/Zoombinis.h
@@ -9,8 +9,15 @@ public:
 	int eyes = 0;
 	int nose = 0;
 	int loco = 0;
+	
+	
+};
+class ZoombiniGroup
+{
+public:
+	int identity = 0;
 	int row = 0;
 	int column = 0;
-	
-	
+
+
 };
--- src/src/ZoombinisConundrum.cpp
+++ src/src/ZoombinisConundrum.cpp
@@ -2,13 +2,145 @@
 //
 
 #include "Zoombinis.h"
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
-    std::array<Zoombini, 16> zoombinis;
+	int n = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int m = 0;
+	int p = 0;
+	int s = 0;
+
+	std::array<Zoombini, 625> zoombinis;
+	std::array<ZoombiniGroup, 16> groupman;
 	
-	zoombinis[0].eyes 
+
+	// zoombinis[0].eyes (Zoombini #1, Eye Feature)
+	zoombinis[n].eyes = a;
+	zoombinis[n].hair = b;
+	zoombinis[n].nose = c;
+	zoombinis[n].loco = d;
+
+	while (n < 625) {
+	//for (int n = 0; n<625; p++){
+
+		// n++;
+		for (int i=0; i < 5; i++) {
+			
+			for (int j=0; j < 5; j++) {
+				
+				for (int k=0; k < 5; k++) {
+					
+					for (int m=0; m < 5; m++) {
+						zoombinis[n].eyes = i;
+						zoombinis[n].hair = j;
+						zoombinis[n].nose = k;
+						zoombinis[n].loco = m;
+						n++;
+					}
+					zoombinis[n].eyes = i;
+					zoombinis[n].hair = j;
+					zoombinis[n].nose = k;
+					zoombinis[n].loco = m;
+					//n++;
+				}
+				zoombinis[n].eyes = i;
+				zoombinis[n].hair = j;
+				zoombinis[n].nose = k;
+				zoombinis[n].loco = m;
+				//n++;
+			}
+			
+			zoombinis[n].eyes = i;
+			zoombinis[n].hair = j;
+			zoombinis[n].nose = k;
+			zoombinis[n].loco = m;
+			//n++;
+		}
+		/*zoombinis[n].eyes = i;
+		zoombinis[n].hair = j;
+		zoombinis[n].nose = k;
+		zoombinis[n].loco = m;
+		n++;*/
+	}
+	// size_t f = sizeof(zoombinis) / sizeof(zoombinis[0]);
+	 
+	for (int f=0; f < 625; f++) {
+		int g = f + 1;
+		//std::cout << "Zoombini Number " << g": "<< zoombinis[f].eyes<< ", "<< zoombinis[f].hair<<", "<< zoombinis[f].nose << ", " << zoombinis[f].loco << std::cout << ". \n";
+	}
+	
+	std::cout << "List of group constituents: \n";
+	for (int q = 0; q < 16; q++) {
+		srand(time(0));
+		groupman[q].identity = (rand() % 625);
+		for (int r = 0; r < q; r++) {
+			if (groupman[q].identity == groupman[r].identity) { //preventing repeat zoombinis
+				groupman[q].identity = (rand() % 625);
+			}
+		}
+		s = groupman[q].identity;
+		
+
+		std::cout << "Member " << (q+1) << ": Zoombini " << groupman[q].identity << " (Features: " << zoombinis[s].eyes << ", " << zoombinis[s].hair << ", " << zoombinis[s].nose << ", " << zoombinis[s].loco << ") \n";
+		
+	}
+	int simils = 0;
+	int eyecheck = 0;
+	int nosecheck = 0;
+	int haircheck = 0;
+	int lococheck = 0;
+	int comps = 0;
+	for (int v = 0; v < 16; v++) {
+		for (int w = 0; w < 16; w++) {
+
+			if (zoombinis[w].eyes == zoombinis[v].eyes) {
+				eyecheck = 1;
+			}
+			else { eyecheck = 0; }
+
+			if (zoombinis[w].hair == zoombinis[v].hair) {
+				haircheck = 1;
+			}
+			else { haircheck = 0; }
+			//else { simils = 0; }
+			if (zoombinis[w].nose == zoombinis[v].nose) {
+				nosecheck = 1;
+			}
+			else { nosecheck = 0; }
+			
+			//else { simils = 0; }
+			if (zoombinis[w].loco == zoombinis[v].loco) {
+				lococheck = 1;
+			}
+			else { lococheck = 0; }
+				//std::cout << "Similarities between " << v + 1 << " and " << w + 1 << ": " << simils << "\n";
+
+
+			
+			simils = eyecheck + haircheck + nosecheck + lococheck;
+		
+			 std::cout << "Similarities between " << v+1 << " and " << w+1 << ": " << simils<< "\n";
+			 std::cout << "Eyes " << eyecheck << ". Nose " << nosecheck << ". Hair " << haircheck << ". Loco " << lococheck << ".\n";
+			 simils = 0;
+			 eyecheck = 0; haircheck = 0; nosecheck = 0; lococheck = 0;
+			
+		}
+		std::cout << "\n Next Zoombini \n";
+
+	}
+
     std::cout << "Hello World!\n";
 }
 
